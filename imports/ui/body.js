f21cf3578d765a5957d9b4978915585a7969fc74
omarc7 <omarch7@gmail.com>
2016-06-07 18:29:01 +0800

    Ranking BM25 done, still needs work on the UI!

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,22 +1,51 @@
 import { Template } from 'meteor/templating';
 
 import { Documents } from '../api/documents.js';
+import { Queries } from '../api/queries.js';
 
 import  './body.html';
 
+Meteor.startup(function () {
+    delete Session.get('documents');
+    delete Session.get('query');
+});
 
 Template.body.helpers({
     documents(){
-        return Documents.find({},{sort:{date:1}});
+        var query = Queries.find({}).fetch()[0];
+        var scores = {};
+        if(query){
+            Session.set('documents', query.results.map(function (result) {
+                return result.document_id;
+            }));
+            query.results.forEach(function (result) {
+                scores[result.document_id] = result.score;
+            });
+        }else{
+            Session.set('documents', []);
+        }
+        var documents = Documents.find({}).fetch();
+        documents.map(function (document) {
+            document.score = scores[document._id];
+            return document;
+        });
+        return documents.sort(function (a, b) {
+            return b.score - a.score;
+        });
     }
 });
 
 Template.body.events({
     'keyup #search': _.debounce(function(e) {
+        Session.set('documents', []);
         Session.set('query', e.target.value);
-    }, 300)
+    }, 300),
+    'submit .query'(event){
+        event.preventDefault();
+    }
 });
 
 Meteor.autorun(function () {
-   Meteor.subscribe('documents', Session.get('query'));
+    Meteor.subscribe('documents', Session.get('documents'));
+    Meteor.subscribe('query', Session.get('query'));
 });
--- server/main.js
+++ server/main.js
@@ -2,74 +2,96 @@ import { Meteor } from 'meteor/meteor';
 import { Documents } from '../imports/api/documents.js';
 import { Dictionary } from '../imports/api/dictionary.js';
 import { Postings } from '../imports/api/postings.js';
+import { Queries } from '../imports/api/queries.js';
 import natural from 'natural';
+import MongoInternals from 'mongodb';
+import math from 'mathjs';
 
 Meteor.startup(() => {
     natural.PorterStemmer.attach();
 });
 
-Meteor.publish('documents', function documentPublication(data) {
+Meteor.publish('query', function queryPublication(data) {
     if(!data){
         return [];
     }
-    var tokens = data.tokenizeAndStem();
-    var terms = Dictionary.find({term: {$in: tokens}}, {fields: {_id:1, freq:1}}).fetch();
-    if(terms.length==0){
-        return [];
+    var tokens = data.tokenizeAndStem().sort();
+    console.log(tokens);
+    var terms = Dictionary.find({term: {$in: tokens}}, {fields: {_id:1, term:1, freq:1, docs: 1}}).fetch();
+    var totalDocsTerms = 0;
+    terms.forEach(function(term){
+        totalDocsTerms += term.docs;
+    });
+    if(Queries.find({query:tokens.join("-"), docs:totalDocsTerms}).count()==0){
+        if(terms.length==0){
+            return [];
+        }
+        var termsDict = {};
+        var pipelineAVDL = [
+            {$group: {_id: "_id", sum: {$sum: "$length"}, total:{$sum:1}}}
+        ];
+        var totalDocs = Documents.aggregate(pipelineAVDL)[0];
+        var N = totalDocs.total;
+        var avdl = calculateAVLD(totalDocs, N);
+
+        terms.forEach(function (term) {
+            var qtf = 0;
+            tokens.forEach(function (token) {
+                qtf += token==term.term ? 1 : 0;
+            });
+            var idf = math.log10((N-term.docs+0.5)/(term.docs+0.5));
+            termsDict[term._id._str] = {idf:idf, qtf: qtf};
+        });
+        var pipelineDocs = [
+            {$match:{"term_id": {$in: terms.map(function (term) {
+                return MongoInternals.ObjectID(term._id._str);
+            })}}},
+            {$group:{_id:"$document_id", terms:{$push:{term_id:"$term_id", freq:"$freq"}}}}
+        ];
+        var documentsTerm = Postings.aggregate(pipelineDocs);
+
+        var documents = Documents.find({_id:{$in:documentsTerm.map(function (documentTerm) {
+            return documentTerm._id;
+        })}}, {_id:1, length: 1}).fetch();
+
+        var documentsDict = {};
+
+        documents.forEach(function (document) {
+            documentsDict[document._id._str] = {length: document.length, score: 0.0};
+        });
+
+        documentsTerm.forEach(function (document) {
+            if(documentsDict.hasOwnProperty(document._id)){
+                documentsDict[document._id].terms = document.terms;
+            }
+        });
+        var scores = [];
+        for(var document_id in documentsDict){
+            if(documentsDict.hasOwnProperty(document_id)){
+                computeBM25(documentsDict[document_id], termsDict, N, avdl);
+                scores.push({document_id: MongoInternals.ObjectID(document_id), score: documentsDict[document_id].score});
+            }
+        }
+        // Queries.insert({query:tokens.join("-"), docs: totalDocsTerms, results:scores});
+        Queries.upsert({query:tokens.join("-")},{$set:{docs:totalDocsTerms, results:scores}});
+        return Queries.find({query:tokens.join("-")});
+    }else{
+        return Queries.find({query:tokens.join("-")});
     }
-    var N = Documents.find().count();
-    var pipelineAVDL = [
-        {$group: {_id: "$document_id", document_length: {$sum: "$freq"}}}
-    ];
-    var avdl = calculateAVLD(Postings.aggregate(pipelineAVDL));
-    var pipelineTermsFreq = [
-        // {$match: {term_id: {
-        //     $in: terms.map(function (term) {
-        //         return term._id;
-        //     })
-        // }}},
-        {$match: {term_id: {$in: terms.map(function (term) {
-            console.log(term._id);
-            return term._id;
-        })}}},
-        // {$group: {_id: "$term_id", term_document:{$sum: 1}}}
-    ];
-    //TODO Fix this aggregation :(
-    console.log(Postings.aggregate(pipelineTermsFreq));
-    var postings = Postings.find({term_id: {$in: terms.map(function (term) {
-        return term._id;
-    })}}).fetch();
-    return Documents.find({_id: {$in: postings.map(function (posting) {
-        return posting.document_id;
-    })}});
 });
 
-var calculateAVLD = function (documents) {
-    var avdl = 0;
-    documents.forEach(function (document) {
-        avdl += document.document_length;
-    });
-    return avdl / documents.length;
+Meteor.publish('documents', function documentsPublication(documents) {
+    return Documents.find({_id:{$in:documents}});
+});
+
+var calculateAVLD = function (result, N) {
+    return result.sum / N;
 }
 
-var computeBM24 = function (documentVector, queryVector, N, avdl, callback) {
-    var rank = 0;
-    callback(rank);
-}
-
-var calculateBM25 = function(documentVector, queryVector, N, avdl, callback){
-    var rank = 0;
-    var dl = getDocumentLength(documentVector); // Document Length
-    documentVector.terms.forEach(function(term){
-        var qtf = queryVector[term.term] >= 1 ? queryVector[term.term] : 0;
-        if(qtf>0){
-            var n = term.docs; //Number of documents that contain the term
-            var w = Math.log((n+0.5)/(N-n+0.5)) / Math.LN10;
-            var k1 = 1.5, b = 0.75, k3 = 500; // Constants
-            var K = k1*((1-b)+(b*(dl/avdl)));
-            var tf = term.docFreq; // Term's frequency in document
-            rank += w * (((k1 + 1) * tf)/(K+tf)) * (((k3 + 1) * qtf)/(k3 + qtf));
-        }
+var computeBM25 = function (documentDict, termsDict, N, avdl) {
+    documentDict.terms.forEach(function (term) {
+        var k1 = 1.5, b = 0.75, k3 = 500; //Constants
+        var K = k1 * ((1-b)+(b*(documentDict.length/avdl)));
+        documentDict.score += termsDict[term.term_id].idf * (((k1 + 1) * term.freq) / (K+term.freq)) * (((k3 + 1) * termsDict[term.term_id].qtf)/(k3 + termsDict[term.term_id].qtf));
     });
-    callback(rank);
 }
